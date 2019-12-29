e507da305a23af13d4839796579c1621a2fef7e0
Chris Thomas <tchrs@uber.com>
2019-12-29 13:15:48 -0500

    2019 - 14 - part2

--- 2018/14/index.js
+++ 2018/14/index.js
@@ -27,40 +27,36 @@ function part1() {
 }
 
 function part2() {
-    const data = expandingArray();
-    data.add(3);
-    data.add(7);
-
-    let elf1Index = 0;
-    let elf2Index = 1;
 
     const target = '864801';
 
-    // for(let i = 0; i < target + 10; i++) {
-    let e = 0;
-    while(true) {
-        // if (e++ > 40) { break; }
+    let board = buffer(2);
+    board.add(3);
+    board.add(7);
 
-        const current = data.last(target.length);
-        // console.log(current);
-        if (current === target) {
-            console.log('found', data.length - target.length);
-            break;
-        }
+    let elfIndex1 = 0;
+    let elfIndex2 = 1;
 
-        const recipe1 = data.get(elf1Index);
-        const recipe2 = data.get(elf2Index);
-        const sum = recipe1 + recipe2;
+    const valueAt = index => board.get(index);
+    const endsWith = target => board.last(target.length) === target;
+    const t = Date.now();
+
+    while(!endsWith(target)) {
+        const sum = valueAt(elfIndex1) + valueAt(elfIndex2);
         for(let c of String(sum)) {
-            data.add(parseInt(c));
+            board.add(parseInt(c));
+            if(endsWith(target)) {
+                break;
+            }
         }
-
-        elf1Index = data.index(elf1Index + 1 + recipe1);
-        elf2Index = data.index(elf2Index + 1 + recipe2);
+        elfIndex1 = (elfIndex1 + 1 + valueAt(elfIndex1)) % board.length;
+        elfIndex2 = (elfIndex2 + 1 + valueAt(elfIndex2)) % board.length;
     }
 
-    // const result = Array.from(data.slice(target, target+10)).join('');
-    // console.log(result);
+    console.log('\nPart II');
+    console.log('Index', board.length - target.length);
+    console.log(` ${Date.now() - t}ms`);
+
 }
 
 // part1();
@@ -82,6 +78,27 @@ function print(data, elf1, elf2) {
     }
     return text;
 }
+
+function buffer(size) {
+    let data = new Array(size);
+    let length = 0;
+
+    return {
+        get length() { return length; },
+        get size() {return size; },
+        last(l) {
+            return data.slice(length - l, length).join('');
+        },
+        get(index) { return data[index]; },
+        add(value) {
+            if (length + 1 > size) {
+                data.length = data.length * 2;
+                size = data.length;
+            }
+            data[length++] = value;
+        }
+    };
+};
 
 function expandingArray() {
     const data = new Map();
