c4ee54c6c0ee15ca6ad318f25bd1d99c54f337e1
Chris Thomas <tchrs@uber.com>
2020-12-23 16:28:01 -0500

    2020/23 - part 2

--- 2020/23/index.js
+++ 2020/23/index.js
@@ -1,28 +1,18 @@
-import { find } from 'lodash';
-import {loadInput,range} from '../../utils';
+import {loadInput,maxOf,range} from '../../utils';
 
-function part1(input) {
-    let {cups, moves} = parseInput(input);
+function part1(input, moves) {
+    let cups = parseInput(input);
 
-    const MOVES = 100;
     const LENGTH = cups.length;
 
     let currentIndex = 0;
 
-    console.log(at(cups, 2));
-
-    for(let i = 0; i < MOVES; i++) {
-        // console.group('turn', i+1);
+    for(let i = 0; i < moves; i++) {
         let current = cups[currentIndex];
-        console.log(i, 'cups', print(cups, current));
 
         const m = pick3(cups, currentIndex);
-        // console.log('current', current);
-        // console.log('picked', m.values);
         const dest = findDestination(m.remaining, current);
-        // console.log('dest', dest);
         const destIndex = m.remaining.indexOf(dest);
-        // console.log('destIndex', destIndex);
 
         m.remaining.splice(
             Math.min(destIndex + 1, m.remaining.length),
@@ -33,7 +23,6 @@ function part1(input) {
 
         currentIndex = (cups.indexOf(current) + 1) % LENGTH;
     }
-    console.log(print(cups));
 
     const offset = cups.indexOf(1) + 1;
     const result = range(0, LENGTH - 2).map(i => at(cups, i + offset)).join('');
@@ -49,10 +38,6 @@ function findDestination(cups, value, exclude = []) {
     }
 }
 
-function print(arr, current) {
-    return arr.map(m => m === current ? `(${m})` : `${m}`).join(' ');
-}
-
 function pick3(cups, index) {
     const values = range(1,3).map(i => at(cups, index + i));
     const remaining = cups.filter(c => !values.includes(c));
@@ -66,120 +51,116 @@ function at(arr, index) {
     return arr[index % arr.length];
 }
 
-function part2(input) {
-
-    // const {cups: starting} = parseInput(input);
-    // let cups = Array.from(new Array(1_000_000), (x, i) => {
-    //     return i < starting.length ? starting[i] : 10 + i - starting.length;
-    // });
-    const cups = [3,8,9,1,2,5,4,6,7];
-
-    const MOVES = 10;
-    const LENGTH = cups.length;
-
-    let currentIndex = 0;
-
-    // console.clear();
-
-    let lastTime = Date.now();
-    for(let i = 0; i < MOVES; i++) {
-        let current = cups[currentIndex];
-
-        console.log(print(cups, current));
-        if (i % 1_000 === 0) {
-            console.log(i, Date.now() - lastTime);
-            lastTime = Date.now();
+class Node {
+    constructor(value) {
+        this.value = value;
+    }
+    move(offset) {
+        if (offset === 0) return this;
+        const key = offset < 0 ? 'prev' : 'next';
+        let cur = this;
+        for(let i =0; i < Math.abs(offset); i++) {
+            cur = cur[key];
         }
+        return cur;
+    }
+}
+function buildList(cups) {
+    let head = new Node(-1);
+    const lookup = new Map();
 
-        const next3 = range(1,3).map(i => at(cups, currentIndex + i));
+    let cur = head;
+    cups.forEach(v => {
+        const newNode = new Node(v);
+        newNode.prev = cur;
+        cur.next = newNode;
+        lookup.set(v, cur.next);
+        cur = cur.next;
+    });
+    const last = lookup.get(cups[cups.length - 1]);
+    last.next = head.next;
+    head.next.prev = last;
 
-        const destination = findDestination(cups, current, next3);
-        const destinationIndex = cups.indexOf(destination);
+    return {
+        head,
+        lookup
+    };
+}
 
-        const cache = slice(cups, currentIndex + 1, destinationIndex + 1);
+function part2(input, moves) {
 
-        for(let o = 0; o < cache.length; o++) {
-            set(cups, currentIndex + 1 + o, at(cache, o+3));
+    const starting = parseInput(input);
+    let cups = Array.from(new Array(1_000_000), (x, i) => {
+        return i < starting.length ? starting[i] : 10 + i - starting.length;
+    });
+    let max = maxOf(cups, x => x);
+
+    const {head, lookup} = buildList(cups);
+
+    const findDestinationNode = (current, exclude) => {
+
+        let value = current.value - 1;
+        if (value < 1) {
+            value = max;
         }
+        while(exclude.includes(value)) {
+            value --;
+            if (value < 1) value = max;
+        }
+        return lookup.get(value);
+    };
 
-        currentIndex = (cups.indexOf(current) + 1) % LENGTH;
+    function remove(base, length) {
+        const oldNode = base.next;
+        const next = base.move(length + 1);
+        next.prev.next = null;
+        next.prev = base;
+        base.next = next;
 
+        return oldNode;
     }
-    console.log(print(cups));
-    console.log();
 
-    const offset = cups.indexOf(1);
-    return at(cups, offset+1) * at(cups, offset+2);
-}
-function set(arr, index, value) {
-    arr[index % arr.length] = value;
-}
-function slice(arr, start, end) {
-    const length = end < start
-        ? (arr.length + end) - start
-        : end - start;
-    // console.log('slice', start, end, length);
-    const data = new Array(length);
-    for(let i =0; i < length; i++) {
-        data[i] = at(arr, start+i);
+    function insert(base, node, length = 1) {
+        const old = base.next;
+        base.next = node;
+        node.prev = base;
+
+        const last = node.move(length - 1);
+        last.next = old;
+        old.prev = last;
     }
-    return data;
-    // if(end < start) {
-    //     return [...arr.slice(start), ...arr.slice(0, end - arr.length)];
-    // }
-    // return arr.slice(start, end);
-}
-function shiftLeft(arr, offset, index, length) {
-    console.log('shiftLeft', offset, index, length);
 
-    const start = index - offset;
-    const cache = range(index - offset, index + length - 1)
-        .map(i => at(arr, i));
-    console.log(' cache', cache);
-    for(let i = 0; i < cache.length; i++) {
-
-        const newValue = at(cache, i + offset);
-        console.log(' set', start +i, newValue);
-        arr[start + i] = newValue;
+    function valuesOf(node, length) {
+        let values = [];
+        let n = node;
+        for(let i = 0; i < length; i++) {
+            values.push(n.value);
+            n = n.next;
+        }
+        return values;
     }
-}
-// function shiftLeft(arr, offset, index, length) {
-//     console.log('shiftLeft', index, offset, length);
-//     if (offset === 0) return;
-//     const start = (index - offset) % arr.length;
-//     // const cache = arr.slice(index - length, index);
-//     for(let i = 0; i < length; i++) {
-//         swap(arr, start+i, index+i);
-//     }
-//     // for(let i = 0; i < cache.length; i++) {
-//     //     arr[index+i] = cache[i];
-//     // }
-// }
 
-function indexDiff(arr, i1, i2) {
-    if (i2 < i1) {
-        i2+= arr.length;
+    let current = head;
+
+    for(let i = 0; i < moves; i++) {
+
+        current = current.next;
+        const pick3 = remove(current, 3);
+        const destination = findDestinationNode(current, valuesOf(pick3, 3));
+        insert(destination, pick3, 3);
     }
-    return i2 - i1;
-}
-function swap(arr, i1, i2) {
-    i1 = i1 % arr.length;
-    i2 = i2 % arr.length;
-    const tmp = arr[i1];
-    arr[i1] = arr[i2];
-    arr[i2] = tmp;
+
+    const node1 = lookup.get(1);
+    const v = valuesOf(node1.next, 2);
+    return v[0] * v[1];
 }
 
 function parseInput(input) {
-    const parts = input.split(',');
-    return {
-        cups: parts[0].split('').map(x => parseInt(x, 10)),
-        moves: parseInt(parts[1], 10),
-    };
+    return input.split('').map(x => parseInt(x, 10));
 }
 
 (function solve() {
     const input = loadInput(2020, 23);
-    // console.log('Part I :', part1(input));
-    console.log('Part II:', part2(input));
+    console.log('Part I :', part1(input, 100));
+    console.log('Part II:', part2(input, 10_000_000));
 })();
