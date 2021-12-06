989d66732720e20c94b8f9ea677bc5ac5974725d
Chris Thomas <tchrs@uber.com>
2021-12-05 23:23:04 -0500

    allow visualizeGrid to take a grid

--- utils/grid.js
+++ utils/grid.js
@@ -1,52 +1,54 @@
 import {minBy, maxBy} from 'lodash';
 
 export function* pointsWithin(item) {
-    for(let z = item.zMin; z <= item.zMax; z++) {
-        for(let y = item.top; y <= item.bottom; y++) {
-            for(let x = item.left; x <= item.right; x++) {
-                yield [x, y, z];
-            }
-        }
+  for(let z = item.zMin; z <= item.zMax; z++) {
+    for(let y = item.top; y <= item.bottom; y++) {
+      for(let x = item.left; x <= item.right; x++) {
+        yield [x, y, z];
+      }
     }
+  }
 }
 
-export function visualizeGrid(bounds, render) {
-    const lines = [];
+export function visualizeGrid(gridOrBounds, render) {
+  const lines = [];
+  // Allow the caller to pass a grid or bounds
+  const bounds = gridOrBounds.bounds || gridOrBounds;
 
-    for(let y = bounds.top; y <= bounds.bottom; y++) {
-        let line = '';
-        for(let x = bounds.left; x <= bounds.right; x++) {
-            line += render(x, y);
-        }
-        lines.push(line);
+  for(let y = bounds.top; y <= bounds.bottom; y++) {
+    let line = '';
+    for(let x = bounds.left; x <= bounds.right; x++) {
+      line += render(x, y);
     }
-    return lines.join('\n');
+    lines.push(line);
+  }
+  return lines.join('\n');
 }
 
 export const maxOf = (arr, accessor) => accessor(maxBy(arr, accessor));
 export const minOf = (arr, accessor) => accessor(minBy(arr, accessor));
 
 export function boundsOfGrid(grid) {
-    return {
-        left: 0,
-        right: grid[0].length - 1,
-        top: 0,
-        bottom: grid.length - 1,
-        width: grid[0].length,
-        height: grid.length
-    };
+  return {
+    left: 0,
+    right: grid[0].length - 1,
+    top: 0,
+    bottom: grid.length - 1,
+    width: grid[0].length,
+    height: grid.length
+  };
 }
 export function findBounds(input, accessX = p => p[0], accessY = p => p[1]) {
-    return {
-        left: minOf(input, accessX),
-        right: maxOf(input, accessX),
-        top: minOf(input, accessY),
-        bottom: maxOf(input, accessY),
-        get width() {
-            return this.right - this.left +1;
-        },
-        get height() {
-            return this.bottom - this.top + 1;
-        }
-    };
+  return {
+    left: minOf(input, accessX),
+    right: maxOf(input, accessX),
+    top: minOf(input, accessY),
+    bottom: maxOf(input, accessY),
+    get width() {
+      return this.right - this.left +1;
+    },
+    get height() {
+      return this.bottom - this.top + 1;
+    }
+  };
 }
